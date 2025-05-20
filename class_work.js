9692e828a602631cd56006f4c1323608fb869d1c
Paul Ifemene <ifemenep@gmail.com>
2025-05-20 06:48:35 +0300

    Upclass_work.js

--- class_work.js
+++ class_work.js
@@ -18,3 +18,108 @@ celebrateWin("Fire Challenge");
 for (let i = 1; i <= 3; i+=1) {
     console.log("The tech Queens have solved Challenge" + i )
 }
+
+let queenName = "Amarachi";
+
+let completedChallenges = 0
+
+completedChallenges += 1;
+console.log(completedChallenges);
+
+
+
+function celebrateWin(challengeName) {
+    console.log("Challenge: " + challengeName + " completed!")
+    console.log("Time to take on the next Challenge!")
+}
+
+celebrateWin("Fire Challenge");
+
+
+for (let i = 1; i <= 3; i+=1) {
+    console.log("The tech Queens have solved Challenge" + i )
+}
+
+let queenName = "Amarachi";
+
+let completedChallenges = 0
+
+completedChallenges += 1;
+console.log(completedChallenges);
+
+
+
+function celebrateWin(challengeName) {
+    console.log("Challenge: " + challengeName + " completed!")
+    console.log("Time to take on the next Challenge!")
+}
+
+celebrateWin("Fire Challenge");
+
+
+for (let i = 1; i <= 3; i+=1) {
+    console.log("The tech Queens have solved Challenge" + i )
+}
+
+let queenName = "Amarachi";
+
+let completedChallenges = 0
+
+completedChallenges += 1;
+console.log(completedChallenges);
+
+
+
+function celebrateWin(challengeName) {
+    console.log("Challenge: " + challengeName + " completed!")
+    console.log("Time to take on the next Challenge!")
+}
+
+celebrateWin("Fire Challenge");
+
+
+for (let i = 1; i <= 3; i+=1) {
+    console.log("The tech Queens have solved Challenge" + i )
+}
+
+let queenName = "Amarachi";
+
+let completedChallenges = 0
+
+completedChallenges += 1;
+console.log(completedChallenges);
+
+
+
+function celebrateWin(challengeName) {
+    console.log("Challenge: " + challengeName + " completed!")
+    console.log("Time to take on the next Challenge!")
+}
+
+celebrateWin("Fire Challenge");
+
+
+for (let i = 1; i <= 3; i+=1) {
+    console.log("The tech Queens have solved Challenge" + i )
+}
+
+let queenName = "Amarachi";
+
+let completedChallenges = 0
+
+completedChallenges += 1;
+console.log(completedChallenges);
+
+
+
+function celebrateWin(challengeName) {
+    console.log("Challenge: " + challengeName + " completed!")
+    console.log("Time to take on the next Challenge!")
+}
+
+celebrateWin("Fire Challenge");
+
+
+for (let i = 1; i <= 3; i+=1) {
+    console.log("The tech Queens have solved Challenge" + i )
+}
