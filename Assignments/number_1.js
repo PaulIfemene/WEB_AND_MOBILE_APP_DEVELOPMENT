2d99a5ba15e0d33e76b93688ba8a54b114a90740
Paul Ifemene <ifemenep@gmail.com>
2025-05-28 21:20:50 +0300

    Update number_1.js

--- Assignments/number_1.js
+++ Assignments/number_1.js
@@ -54,3 +54,118 @@ let stdno = "M24B13034"
 console.log(greeting(userName));
 
 console.log(stdno);
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
+
+// Number 1
+// Write a function that takes a user's name and greets the user in your local language (Igbo)
+function greeting(name) {
+    return `Nnọọ, ${name}! Kedu ka ị mere?`; 
+}
+
+let userName = "Chukwuma";
+
+let stdno = "M24B13034"
+
+console.log(greeting(userName));
+
+console.log(stdno);
