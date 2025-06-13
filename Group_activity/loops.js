5e2b57a3991a21dfdf0ab692960b9fd1faf37046
Paul Ifemene <ifemenep@gmail.com>
2025-06-13 22:29:54 +0300

    Update loops.js

--- Group_activity/loops.js
+++ Group_activity/loops.js
@@ -9,3 +9,33 @@ for (let i = 1; i <= 10; i++) {
 }
 
 // Exercise 3
+
+
+
+
+// Excercise 1
+for( let i = 1; i <= 10; 1++ ) {
+    console.log(i);
+}
+
+// Exercise 2
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 x ${i} = ${5 * i}`);
+}
+
+// Exercise 3
+
+
+
+// Excercise 1
+for( let i = 1; i <= 10; 1++ ) {
+    console.log(i);
+}
+
+// Exercise 2
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 x ${i} = ${5 * i}`);
+}
+
+// Exercise 3
+
