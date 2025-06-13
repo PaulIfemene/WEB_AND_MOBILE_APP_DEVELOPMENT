1276aad3d65156582ae8918f84da2f7ce4d3ccd7
Paul Ifemene <ifemenep@gmail.com>
2025-06-13 22:29:21 +0300

    Update testing.js

--- Group_activity/testing.js
+++ Group_activity/testing.js
@@ -11,3 +11,34 @@ if(celsius => 30){
 } else {
     console.log("It's cold");
 }
+
+
+
+// Exercise 1
+let user_input = prompt("Please enter the value for temperature in Celsius: ");
+
+// converting user_input to float
+let celsius = parseFloat(user_input);
+
+if(celsius => 30){
+    console.log("It's too hot");
+} else if(celsius => 20){
+    console.log("It's moderate");
+} else {
+    console.log("It's cold");
+}
+
+
+// Exercise 1
+let user_input = prompt("Please enter the value for temperature in Celsius: ");
+
+// converting user_input to float
+let celsius = parseFloat(user_input);
+
+if(celsius => 30){
+    console.log("It's too hot");
+} else if(celsius => 20){
+    console.log("It's moderate");
+} else {
+    console.log("It's cold");
+}
