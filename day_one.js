8cc000f5a35e85ba38cb5fe116dcbb620c408013
Paul Ifemene <ifemenep@gmail.com>
2025-07-09 23:33:26 +0300

    Update day_one.js

--- day_one.js
+++ day_one.js
@@ -27,4 +27,33 @@ let last_name = "Chukwuma"
 
 let full_name = first_name + " " + last_name
 
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
 console.log(full_name)
