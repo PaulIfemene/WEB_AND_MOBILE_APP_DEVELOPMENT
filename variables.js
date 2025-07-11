8426c5255a512c913c6cca516b19e5cbb071d803
Paul Ifemene <ifemenep@gmail.com>
2025-07-11 13:46:27 +0300

    Update variables.js

--- variables.js
+++ variables.js
@@ -56,3 +56,61 @@ height = 3.4;
 
 console.log(height)
 
+/// this is my first JS program
+
+console.log("hello World")
+//
+console.log("Ifemene Paul Chukwumaobim")
+console.log("B28325")
+console.log("M24B13/034")
+console.log("Bachelors Degree in Information Technology")
+
+my_name =
+height = 3.4;
+
+console.log(height)
+
+/// this is my first JS program
+
+console.log("hello World")
+//
+console.log("Ifemene Paul Chukwumaobim")
+console.log("B28325")
+console.log("M24B13/034")
+console.log("Bachelors Degree in Information Technology")
+
+my_name =
+height = 3.4;
+
+console.log(height)
+
+/// this is my first JS program
+
+console.log("hello World")
+//
+console.log("Ifemene Paul Chukwumaobim")
+console.log("B28325")
+console.log("M24B13/034")
+console.log("Bachelors Degree in Information Technology")
+
+my_name =
+height = 3.4;
+
+console.log(height)
+
+
+
+/// this is my first JS program
+
+console.log("hello World")
+//
+console.log("Ifemene Paul Chukwumaobim")
+console.log("B28325")
+console.log("M24B13/034")
+console.log("Bachelors Degree in Information Technology")
+
+my_name =
+height = 3.4;
+
+console.log(height)
+
