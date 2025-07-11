9cb66a5cda96d1bd12bdf4b9a5f9210fd19945ed
Paul Ifemene <ifemenep@gmail.com>
2025-07-11 13:47:20 +0300

    Update day_one.js

--- day_one.js
+++ day_one.js
@@ -56,4 +56,178 @@ let last_name = "Chukwuma"
 
 let full_name = first_name + " " + last_name
 
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
+console.log(full_name)
+
+
+
+console.log("Hello World");
+
+let first_name = "Paul"
+let last_name = "Chukwuma"
+
+let full_name = first_name + " " + last_name
+
 console.log(full_name)
